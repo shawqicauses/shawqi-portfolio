09362f5680a978f2f128e2dd5b6dd0d11bb68de0
Shawqi Hatem <shawqicauses@outlook.com>
2022-07-31 13:40:47 +0300

    Update: Refactor using Sanity client instead of hard-coded data

--- front-end-react/src/constants/images/index.jsx
+++ front-end-react/src/constants/images/index.jsx
@@ -1,8 +1,3 @@
-import brush from "../../assets/brush-dynamic-color.png"
-import computer from "../../assets/computer-dynamic-color.png"
-import figma from "../../assets/figma-dynamic-color.png"
 import file from "../../assets/file-text-dynamic-color.png"
-import folder from "../../assets/folder-dynamic-color.png"
-
-const images = { file, figma, brush, computer, folder }
+const images = { file }
 export default images
--- front-end-react/src/containers/about/index.jsx
+++ front-end-react/src/containers/about/index.jsx
@@ -1,33 +1,17 @@
-import images from "../../constants/images"
+import { useEffect, useState } from "react"
+import { client, urlForImage } from "../../client"
 import { classes } from "../../utils/utils"
 
 export default function About() {
-  const data = [
-    {
-      image: images.figma,
-      title: "Figma Design Tool",
-      description: [
-        "I use Figma application for years now and I am so grateful that",
-        "I have this tool because it makes your design process easier!"
-      ].join(" ")
-    },
-    {
-      image: images.brush,
-      title: "Web Design",
-      description: [
-        "Web design is so freaking interesting due to its challenges!",
-        "With some design basics and helpful tools you are going to rock"
-      ].join(" ")
-    },
-    {
-      image: images.computer,
-      title: "Web Development",
-      description: [
-        "This is my powerful spot which I also have experienced the most",
-        "Techniques come and go but the solid foundation I have does not!"
-      ].join(" ")
-    }
-  ]
+  const [about, setAbout] = useState([])
+
+  useEffect(() => {
+    const query = ["*", "[_type == 'about']"].join("")
+    client
+      .fetch(query)
+      .then(data => setAbout(data))
+      .catch(error => console.log(error))
+  }, [])
 
   return (
     <section
@@ -84,33 +68,27 @@ export default function About() {
         </div>
         <div
           className={classes(
-            "grid grid-cols-1 gap-10",
-            "md:grid-cols-2 lg:grid-cols-3",
-            "lg:gap-[3.75rem] xl:gap-20"
+            "w-fullc grid h-auto grid-cols-1 gap-10",
+            "content-start items-start justify-center",
+            "xl-2:gap-20 md:grid-cols-2 lg:grid-cols-3 xl:gap-[3.75rem]"
           )}>
-          {data.map((item, index) => (
+          {about.map((item, index) => (
             <div
               key={index}
               className={classes("flex flex-col items-start justify-start")}>
-              <div
+              <img
+                src={urlForImage(item.image)}
+                alt={item.title}
                 className={classes(
-                  "relative flex flex-row items-center justify-center",
-                  "rounded-xl-2 aspect-video w-full bg-zinc-100",
-                  "mb-6 ring-2 ring-zinc-50 ring-offset-8 lg:mb-8"
-                )}>
-                <img
-                  src={item.image}
-                  alt={item.title}
-                  className={classes(
-                    "absolute top-1/2 left-1/2 bottom-auto right-auto",
-                    "w-1/2 -translate-x-1/2 -translate-y-1/2",
-                    "aspect-auto transform object-cover object-center"
-                  )}
-                />
-              </div>
+                  "h-full w-full object-cover object-center",
+                  "aspect-video transform object-cover",
+                  "rounded-xl-2 mb-8 translate-y-0 lg:mb-10"
+                )}
+              />
               <span
                 className={classes(
-                  "font-monospace text-base font-medium leading-none",
+                  "font-monospace text-base font-medium",
+                  "uppercase leading-none tracking-normal",
                   "mb-4 text-zinc-400 md:text-lg lg:mb-6"
                 )}>
                 {index + 1 >= 10 ? index : "0" + (index + 1)}
@@ -118,8 +96,8 @@ export default function About() {
               <h3
                 className={classes(
                   "font-semi-bold text-left text-lg leading-none",
-                  "max-w-xs text-left tracking-tight text-zinc-900",
-                  "mb-4 mt-auto md:text-xl lg:mb-6"
+                  "text-left tracking-tight text-zinc-900",
+                  "mb-4 mt-auto max-w-xs md:text-xl lg:mb-6"
                 )}>
                 {item.title}
               </h3>
